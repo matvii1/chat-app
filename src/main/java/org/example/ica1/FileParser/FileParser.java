9e6de5444ba2fce3937e9fbcbf972d9a692b78ba
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-04-22 12:47:42 +0200

    fixes

--- src/main/java/org/example/ica1/FileParser/FileParser.java
+++ src/main/java/org/example/ica1/FileParser/FileParser.java
@@ -14,7 +14,20 @@ public class FileParser {
   
   public static ArrayList<Message> parseChatFile(File file) throws FileNotFoundException {
     ArrayList<Message> messages = new ArrayList<>();
+    Scanner scannerCheck = new Scanner(file);
     Scanner scanner = new Scanner(file);
+    
+    ArrayList<String> array = new ArrayList<>();
+    while (scannerCheck.hasNextLine()) {
+      array.add(scannerCheck.nextLine());
+    }
+    
+    if (array.isEmpty() || array.getLast().isEmpty()) {
+      Alert alert = new Alert(Alert.AlertType.ERROR, "Error: Last element of the array should be an empty string");
+      alert.show();
+      
+      return new ArrayList<>();
+    }
     
     while (scanner.hasNextLine()) {
       String name = null;
@@ -24,42 +37,42 @@ public class FileParser {
 //      ORDER does matter!!!
       if (scanner.hasNextLine()) {
         String nextLine = scanner.nextLine();
-        String[] arr = nextLine.split(":", 2);
+        String[] arrayLine = nextLine.split(":", 2);
         
-        if (!Objects.equals(arr[0], "Time") || arr[1].isEmpty()) {
-          Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Time format: " + nextLine);
+        if (!Objects.equals(arrayLine[0], "Time") || arrayLine[1].isEmpty()) {
+          Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid format for message: " + nextLine);
           alert.show();
           
           return new ArrayList<>();
         } else {
-          time = arr[1].trim();
+          time = arrayLine[1].trim();
         }
       }
       if (scanner.hasNextLine()) {
         String nextLine = scanner.nextLine();
-        String[] arr = nextLine.split(":", 2);
+        String[] arrayLine = nextLine.split(":", 2);
         
-        if (!Objects.equals(arr[0], "Name") || arr[1].isEmpty()) {
+        if (!Objects.equals(arrayLine[0], "Name") || arrayLine[1].isEmpty()) {
           Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Name format: " + nextLine);
           alert.show();
           
           return new ArrayList<>();
         } else {
-          name = arr[1].trim();
+          name = arrayLine[1].trim();
         }
       }
       
       if (scanner.hasNextLine()) {
         String nextLine = scanner.nextLine();
-        String[] arr = nextLine.split(":", 2);
+        String[] arrayLine = nextLine.split(":", 2);
         
-        if (!Objects.equals(arr[0], "Message")) {
+        if (!Objects.equals(arrayLine[0], "Message")) {
           Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Message format: " + nextLine);
           alert.show();
           
           return new ArrayList<>();
         } else {
-          message = arr[1].trim();
+          message = arrayLine[1].trim();
         }
       }
       
